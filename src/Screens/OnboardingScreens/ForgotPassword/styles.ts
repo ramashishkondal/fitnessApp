6a73759fde133c9b5c60bd79e839ad2aa8a2b3f4
ramashish-kondal <ramashish.kondal@chicmic.co.in>
2024-08-01 19:47:42 +0530

    fix: textAlign Center for forgot password and reset password.

--- src/Screens/OnboardingScreens/ForgotPassword/styles.ts
+++ src/Screens/OnboardingScreens/ForgotPassword/styles.ts
@@ -15,6 +15,7 @@ export const styles = StyleSheet.create({
     fontWeight: SIZES.fontBold1,
   },
   textInput: {
-    textAlign: 'center',
+    // textAlign: 'center',
+    paddingHorizontal: 24,
   },
 });
